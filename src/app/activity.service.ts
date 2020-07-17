7b33ed63247ae48e6f4a2164501d6c0b9fa6e0d5
George Jacob <georgekunchattil@gmail.com>
2020-07-17 22:24:53 +0530

    Implementing APIs Methods

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -11,13 +11,13 @@ export class ActivityService {
   constructor(private _httpClient: HttpClient) { }
 
   //fetch only single Activity in the application
-  getActivity() {
-
+  getActivity(activityID: string) {
+    return this._httpClient.get(API + "/id/"+ activityID); //fetching data from API based on activityID provided by user
   }
 
   //fetch AllActivity avaliable in the application
   getAllActivity() {
-    
+    return this._httpClient.get(API); //fetching data from API
   }
   
 }
